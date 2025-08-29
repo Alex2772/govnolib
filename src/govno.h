09f8f1214e583399191391885c59ffff192dc1a5
alex2772 <alex2772sc@gmail.com>
2025-08-29 16:21:23 +0300

    u

--- src/govno.h
+++ src/govno.h
@@ -1,8 +1,15 @@
 #pragma once
 
 #include <string>
+#include <concepts>
+
 
 namespace govno {
 std::string version();
 std::string test(std::string input);
+
+template<std::integral T>
+void test2(T&& t) {
+
+}
 }
